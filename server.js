6b7fe633fa605a49cc0520be49f0fa420dccae85
disc99 <disc99.g@gmail.com>
2019-02-10 01:56:15 +0900

    Add put endpoint

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ const mongoose = require('mongoose');
 const moment = require("moment");
 const multer = require('multer');
 const path = require('path');
-
 const upDir = path.join(__dirname, 'upload'); 
 const uploadDir = multer({dest: upDir}); 
-
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
@@ -85,6 +82,10 @@ mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${proc
           res.json(next);
         })
       })
+      .put((req, res) => {
+        // TODO
+        res.json(next)
+      })
   }
 );
 
@@ -112,4 +113,3 @@ app.post('/', uploadDir.single('upFile'), (req, res) => {
 app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
-
